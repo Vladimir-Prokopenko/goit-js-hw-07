a83e41bc29bf94aff3a675aa316c567500aed54c
Владимир Прокопенко <profcompanysumy@gmail.com>
2025-02-05 17:42:35 +0200

    task07_01 ok

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+function createItem({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+`;
+}
+
+function createAllgallery(object) {
+  return object.map((item) => createItem(item)).join("");
+}
+
+refs.gallery.innerHTML = createAllgallery(galleryItems);
+
+refs.gallery.addEventListener("click", onClickImageModelZoom);
+
+function onClickImageModelZoom(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const bigImageUrl = event.target.dataset.source;
+  const sizeByWidth = window.innerWidth - 20;
+
+  const instance = basicLightbox.create(
+    `
+      <div class="modal" >
+          <img
+            width=${sizeByWidth}
+            src="${bigImageUrl}"
+              />
+              </div>
+              `
+  );
+
+  instance.show();
+
+  window.addEventListener("keydown", instanceCloseModal);
+
+  function instanceCloseModal(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", instanceCloseModal);
+    }
+  }
+}
