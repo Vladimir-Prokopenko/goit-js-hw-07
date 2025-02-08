26cf3a08cf7ad230ba585acc41eb63ea55799944
Владимир Прокопенко <profcompanysumy@gmail.com>
2025-02-08 08:53:10 +0200

    task 07 0k

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryRefs: document.querySelector(".gallery"),
+};
+
+function creatingGalleryMarkup(arry) {
+  return arry.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`,
+    ""
+  );
+}
+
+refs.galleryRefs.innerHTML = creatingGalleryMarkup(galleryItems);
+
+refs.galleryRefs.addEventListener("click", onClickSimpleLightbox);
+
+function onClickSimpleLightbox(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
